8b0639a574505754e20e75006cf8bf12ebc6ed73
Nikolay <84223580+yavrodiy@users.noreply.github.com>
2026-03-23 23:47:28 +0500

    Update button.c

--- button.c
+++ button.c
@@ -3,23 +3,33 @@
 
 int main (void) {
 
-const char  button_pin = 0;
+const char  sens_on = 1;	//compatibility with PSU control
+const char  button_pin = 8; //connect to 3.3v
 const char  sense_pin = 1;
 const char  out_pin = 2;    //pin connected to external relay module
 
 //  int system();
   wiringPiSetup();
-  pinMode (button_pin, INPUT) ;
-  pullUpDnControl (button_pin, PUD_UP);
-  pinMode (sense_pin, INPUT) ;
+  pinMode (button_pin, INPUT);
+  pullUpDnControl (button_pin, PUD_DOWN);
+  pinMode (sense_pin, OUTPUT);
   pinMode (out_pin, OUTPUT);
-  while (true)
+  while (1)
  {
-	if (digitalRead(button_pin) && digitalRead(sense_pin)) {
+	//sense for psu control   
+    if ( sens_on  &&  !(digitalRead(out_pin))) {
+     digitalWrite(sense_pin, 0);
+    }
+    else if ( sens_on &&  digitalRead(out_pin)) {
+     digitalWrite(sense_pin, 1);
+    }
+
+	//on-off printer
+	if (digitalRead(button_pin) && digitalRead(out_pin)) {
 	 digitalWrite(out_pin, 0);
 	 delay (2000);
 	}
-	else if (digitalRead(button_pin) && !(digitalRead(sense_pin))) {
+	else if (digitalRead(button_pin) && !(digitalRead(out_pin))) {
 	 digitalWrite(out_pin, 1);
 	 delay (2000);
 	}
